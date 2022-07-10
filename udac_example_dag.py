4e03f89591d58ed4832b4eb366da4c94e7103f8e
TanviRastogi <tanshikiet@gmail.com>
2022-07-10 12:49:19 -0400

    Update udac_example_dag.py

--- udac_example_dag.py
+++ udac_example_dag.py
@@ -12,12 +12,13 @@ from airflow.contrib.hooks.aws_hook import AwsHook
 AWS_KEY = AwsHook('aws_credentials').get_credentials().access_key
 AWS_SECRET = AwsHook('aws_credentials').get_credentials().secret_key
 
+
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2018, 11, 1),
     'depends_on_past': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=2),
+    'retry_delay': timedelta(minutes=1),
     'catchup': False,
     'email_on_retry': False
 }
@@ -40,7 +41,7 @@ stage_events_to_redshift = StageToRedshiftOperator(
     table="staging_events",
     s3_bucket="udacity-dend",
     s3_key="log_data/{{execution_date.year}}/{{execution_date.month}}/{{ds}}-events.json",
-    extra_parameter="FORMAT AS JSON 's3://udacity-dend/log_json_path.json'",
+    extra_parameter='s3://udacity-dend/log_json_path.json',
     dag=dag
 )
 
@@ -50,8 +51,8 @@ redshift_conn_id='redshift',
 aws_credentials_id='aws_credentials',
 table='staging_songs',
 s3_bucket='udacity-dend',
-s3_key='song_data/A/A/B/',
-#extra_parameter="FORMAT AS JSON 'auto'",
+s3_key='song_data/',
+extra_parameter='auto',
 dag=dag
 
 )
@@ -67,26 +68,50 @@ load_songplays_table = LoadFactOperator(
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
+    redshift_conn_id='redshift',
+    table_name='users',
+    sql_statement=SqlQueries.user_table_insert,
+    truncate_flag='True',
     dag=dag
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
+    redshift_conn_id='redshift',
+    table_name='songs',
+    sql_statement=SqlQueries.song_table_insert,
+    truncate_flag='True',
     dag=dag
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
+    redshift_conn_id='redshift',
+    table_name='artists',
+    sql_statement=SqlQueries.artist_table_insert,
+    truncate_flag='True',
     dag=dag
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
+    redshift_conn_id='redshift',
+    table_name='time',
+    sql_statement=SqlQueries.time_table_insert,
+    truncate_flag='True',
     dag=dag
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id="redshift",
+    dq_checks=    [{'check_sql': "SELECT COUNT(*) FROM users WHERE userid is null", 'expected_result':0},
+                    {'check_sql': "SELECT COUNT(*) FROM songs WHERE songid is null", 'expected_result':0},
+                    {'check_sql': "SELECT COUNT(*) FROM artists WHERE artistid is null", 'expected_result':0},        
+                    {'check_sql': "SELECT COUNT(*) FROM time WHERE start_time is null", 'expected_result':0},
+                    {'check_sql': "SELECT COUNT(*) FROM songplays WHERE userid is null", 'expected_result':0}] ,
+    tables=('songplays', 'songs', 'users', 'artists', 'time'),
     dag=dag
 )
 
@@ -106,3 +131,4 @@ load_song_dimension_table>>run_quality_checks
 load_artist_dimension_table>>run_quality_checks
 load_time_dimension_table>>run_quality_checks
 run_quality_checks>>end_operator
+
